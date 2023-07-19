b0f119da8ce3948c71fbc2ffaa17e8768d95f331
veepgoose <victoriaplatt@loveplanes.co.uk>
2023-07-19 20:50:03 +0100

    Changes to index.js after running build on front end repo

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const apiRoutes = require('./routes/api');
+const path = require('path'); // Add this line
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,6 +11,14 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/api', apiRoutes);
+
+// Serve static files from the build folder
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Catch-all route to serve the index.html file from the build folder
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
