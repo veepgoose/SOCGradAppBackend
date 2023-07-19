ea24aceebb8f290c72085ccd71777eca6860b2d9
veepgoose <victoriaplatt@loveplanes.co.uk>
2023-07-19 21:32:14 +0100

    Continuing to debug issues with Heroku deployment

--- src/index.js
+++ src/index.js
@@ -1,25 +1,64 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+const cors = require('cors'); // Import the cors package
+
 const apiRoutes = require('./routes/api');
-const path = require('path'); // Add this line
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors()); // Use cors middleware to allow requests from all origins
 app.use(bodyParser.json());
 
 app.use('/api', apiRoutes);
 
-// Serve static files from the build folder
-app.use(express.static(path.join(__dirname, 'build')));
-
-// Catch-all route to serve the index.html file from the build folder
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const apiRoutes = require('./routes/api');
+// const path = require('path'); // Add this line
+
+// const app = express();
+// const port = process.env.PORT || 5000;
+
+// app.use(cors());
+// app.use(bodyParser.json());
+
+// app.use('/api', apiRoutes);
+
+// // Serve static files from the build folder
+// app.use(express.static(path.join(__dirname, 'build')));
+
+// // Catch-all route to serve the index.html file from the build folder
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}`);
+// });
